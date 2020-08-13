504aedf1a387b161939a74d94fa804e52d0d3e82
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-13 11:56:32 -0400

    set up classes

---
+++ ATM.java
@@ -0,0 +1,9 @@
+// Tutorial ref: https://www.youtube.com/watch?v=k0BofouWX-o&feature=emb_title
+package com.company;
+
+public class ATM {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
